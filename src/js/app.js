784058f6161b08d339d35be302e601e852482067
mariemnasri06 <mariemnasri06@gmail.com>
2021-12-12 22:33:40 +0100

    commit all changes (add candidates, update accout section , add modal ... )

--- src/js/app.js
+++ src/js/app.js
@@ -1,110 +1,162 @@
+var allowedAccounts;
+
 App = {
   web3Provider: null,
   contracts: {},
-  account: '0x0',
-
-  init: function() {
+  account: "0x0",
+  init: function () {
     return App.initWeb3();
   },
+  initWeb3: function () {
+    const metaProvider = web3.currentProvider;
+    App.web3Provider = metaProvider;
+    web3 = new Web3(web3.currentProvider);
+    App.loadAccountData();
 
-  initWeb3: function() {
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
+    App.web3Provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+    web3 = new Web3(App.web3Provider);
+
+    web3.eth.getAccounts((err, accounts) => {
+      allowedAccounts = accounts.slice(0, 2);
+      console.log(allowedAccounts);
+      App.web3Provider = metaProvider;
       web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      web3 = new Web3(App.web3Provider);
-    }
+    });
+
+    ethereum.on("accountsChanged", (a) => {
+      App.account = a[0];
+      App.updateAccountSection();
+      App.render();
+    });
     return App.initContract();
   },
-
-  initContract: function() {
-    $.getJSON("Election.json", function(election) {
+  initContract: function () {
+    $.getJSON("Election.json", function (election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
-      //App.listenForEvents();
       return App.render();
     });
   },
-
-  render: function() {
-  var electionInstance;
-  var loader = $("#loader");
-  var content = $("#content");
-
-  loader.show();
-  content.hide();
-
-  // Load account data
-  web3.eth.getCoinbase(function(err, account) {
-    if (err === null) {
-      App.account = account;
-      $("#accountAddress").html("Your Account: " + account);
+  loadAccountData: function () {
+    // Load account data
+    web3.eth.getCoinbase(function (err, account) {
+      if (err === null) {
+        App.account = account;
+        App.updateAccountSection();
+      }
+    });
+  },
+  updateAccountSection: function () {
+    var accountSection = $("#account-section");
+    accountSection.empty();
+    accountSection.append(
+      '<p id="accountAddress">Your Account:' +
+        "<strong>" +
+        App.account +
+        "</strong></p>"
+    );
+    if (allowedAccounts.includes(App.account)) {
+      accountSection.append(
+        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addModal">' +
+          "Add Condidate" +
+          "</button>"
+      );
     }
-  });
-
-  // Load contract data
-  App.contracts.Election.deployed().then(function(instance) {
-    electionInstance = instance;
-    return electionInstance.candidatesCount();
-  }).then(function(candidatesCount) {
-    var candidatesResults = $("#candidatesResults");
-    candidatesResults.empty();
-
-    var candidatesSelect =$("#candidatesSelect");
-    candidatesSelect.empty();
-  
-    for (var i = 1; i <= candidatesCount; i++) {
-      electionInstance.candidates(i).then(function(candidate) {
-        var id = candidate[0];
-        var name = candidate[1];
-        var voteCount = candidate[2];
-
-        // Render candidate Result
-        var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-        candidatesResults.append(candidateTemplate);
-
-        // Render candidate ballot option
-        var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-        candidatesSelect.append(candidateOption);
+  },
+  render: function () {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+    loader.show();
+    content.hide();
+    // Load contract data
+    App.contracts.Election.deployed()
+      .then(function (instance) {
+        electionInstance = instance;
+        return electionInstance.candidatesCount();
+      })
+      .then(function (candidatesCount) {
+        var candidatesResults = $("#candidatesResults");
+        candidatesResults.empty();
+        var candidatesSelect = $("#candidatesSelect");
+        candidatesSelect.empty();
+        for (var i = 1; i <= candidatesCount; i++) {
+          electionInstance.candidates(i).then(function (candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
+            // Render candidate Result
+            var candidateTemplate =
+              "<tr><th>" +
+              id +
+              "</th><td>" +
+              name +
+              "</td><td>" +
+              voteCount +
+              "</td></tr>";
+            candidatesResults.append(candidateTemplate);
+            candidatesSelect.append(
+              "<option value='" + id + "'>" + name + "</option>"
+            );
+          });
+        }
+        return electionInstance.voters(App.account);
+      })
+      .then((hasVoted) => {
+        $("#vote-form").show();
+        if (hasVoted) {
+          $("#vote-form").hide();
+        }
+        loader.hide();
+        content.show();
+      })
+      .catch(function (error) {
+        console.warn(error);
       });
-    }
-    return electionInstance.voters(App.account);
-  }).then(function(hasVoted) {
-    // Do not allow a user to vote
-    if(hasVoted) {
-      $('form').hide();
-    }
-    loader.hide();
-    content.show();
-  }).catch(function(error) {
-    console.warn(error);
-  });
-},
 
- castVote:function() {
-  var candidateId =$("#candidateSelect").val();
-  App.contracts.Election.deployed()
-  .then(function(instance){
-    return instance.vote(candidateId, {form :App.account})
-  }).then(function(result){
-    //Wait for votes to update 
-    $("#content").hide();
-    $("#loader").show();
-  }).catch(function(err){
-    console.error(err)
-  })
-}
+    $("#submit-btn").attr("disabled", "disabled");
+
+    $("#nameInput").on("input", () => {
+      if ($("#nameInput").val() !== "") {
+        $("#submit-btn").removeAttr("disabled");
+      } else {
+        $("#submit-btn").attr("disabled", "disabled");
+      }
+    });
+  },
+  castVote: function () {
+    var candidateId = $("#candidatesSelect").val();
+    App.contracts.Election.deployed()
+      .then((instance) => {
+        return instance.vote(candidateId, { from: App.account });
+      })
+      .then(() => {
+        App.render();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  addCandidate: function () {
+    var name = $("#nameInput").val();
+    App.contracts.Election.deployed()
+      .then((instance) => {
+        return instance.addCandidate(name, { from: App.account });
+      })
+      .then(() => {
+        App.render();
+        $("#addModal").modal("hide");
+        $("#nameInput").val("");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
 };
-
-
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
-
